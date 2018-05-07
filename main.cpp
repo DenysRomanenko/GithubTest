05c540278f37b4e62785ee285ddefa4015e9adc7
DenysRomanenko <38042051+DenysRomanenko@users.noreply.github.com>
2018-05-07 22:13:59 +0300

    Update main.cpp
    dasda

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 int main(){
 	constexpr int b = 3;
-<<<<<<< HEAD
-=======
 	int fetch = 2;
-	int c = 4;
-	int a = 3;
-	int d = 3;
 	int l = 5;
->>>>>>> 53317eefdd33dc68974a918a72943aa5d8adeff4
 	return b;
 }
